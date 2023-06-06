20c602d8c38bd8af2ea5d3035af61f91c2c886bb
Luke Stoodley <luke@ichigo.uk>
2023-06-06 17:52:30 +0100

    implement authentication handling

--- src/handlers/message.js
+++ src/handlers/message.js
@@ -3,21 +3,28 @@
  * @param {*} message JSON Message from gateway
  */
 function handleMessage (message) {
-  try { 
-    message = JSON.parse(message)
+  try {
+    message = JSON.parse(message);
   } catch {
-    this.log('error', 'Message failed to parse:', message)
+    this.log('error', 'Message failed to parse:', message);
   }
 
-  switch(message.e) {
+  switch (message.e) {
     case 'HEARTBEAT': {
-      this.ws.send(JSON.stringify({ t: 2, e: 'HEARTBEAT' }))
-      this.log('debug', 'Sent heartbeat', '(Ping:', Date.now() - message.d, 'ms)')
+      this.ws.send(JSON.stringify({ t: 2, e: 'HEARTBEAT' }));
+      this.log('debug', 'Sent heartbeat', '(Ping:', Date.now() - message.d, 'ms)');
+    } break;
+    case 'AUTHENTICATE': {
+      this.log('debug', 'Authetication requested from gateway... trying');
+      this.ws.send(JSON.stringify({ t: 0, e: 'AUTHENTICATE', d: this.token }));
+    } break;
+    case 'AUTHENTICATION_SUCCESS': {
+      this.log('debug', 'Successfully authenticated with the cryostat gateway');
     } break;
     default: {
-      this.log('debug', 'Message recieved:', message)
+      this.log('debug', 'Message recieved:', message);
     }
   }
 }
 
-module.exports = handleMessage
+module.exports = handleMessage;
